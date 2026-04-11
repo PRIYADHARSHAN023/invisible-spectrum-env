a5fae4b9c5922e12ffb36c887cc31a574dfccf79
PRIYADHARSHAN023 <priyadharshan023@example.com>
2026-04-11 11:05:46 +0530

    fix main function validator error

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from ise_env import InvisibleSpectrumEnv
 
@@ -14,3 +15,9 @@ def reset():
 def step(action: dict):
     result = env.step(action)
     return result
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+
+if __name__ == '__main__':
+    main()
